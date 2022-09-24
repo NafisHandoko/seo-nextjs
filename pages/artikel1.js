72d310265300152915218008affef4ac46f23abe
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2022-09-24 20:34:48 +0700

    Add files via upload

---
+++ pages/artikel1.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Artikel1() {
+    return (
+        <div>
+            <Head>
+                <title>Cara mengetik cepat</title>
+            </Head>
+            <h1>Cara mengetik cepat</h1>
+        </div>
+    )
+}
---
+++ pages/artikel2.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Artikel2() {
+    return (
+        <div>
+            <Head>
+                <title>Cara membangun rumah</title>
+            </Head>
+            <h1>Cara membangun rumah</h1>
+        </div>
+    )
+}
